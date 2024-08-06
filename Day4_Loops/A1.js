1390603be7dd6a5cf2628068268181e97506af08
= <dev.abidkk@gmail.com>
2024-08-06 05:29:52 -0700

    loops

--- Day4_Loops/A1.js
+++ Day4_Loops/A1.js
@@ -5,8 +5,8 @@ for (let i = 0; i <= 10; i++){
     console.log(`Number: ${i}`);
 }
 
-// Task2: print table of 5
 
+// Task2: print table of 5
 for (let i = 1; i <= 10; i++){
     console.log(`5 x ${i} = ${i*5}`);
 }
