1c260919517d48e21e723a525f7f88b994765d8f
= <dev.abidkk@gmail.com>
2024-08-06 10:58:06 -0700

    default paramters

---
+++ Day8_ES6-Features/A4.js
@@ -0,0 +1,9 @@
+// Default Parameters
+
+// Task 7: write a function that takes two paramters and return their product, second parameter having default value 1, 
+let product = (v1, v2 = 2)=> {
+    return v1*v2
+} 
+console.log(product(30));
+
+
