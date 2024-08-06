54882774f2d26cb23ceff9b7bba00692e7ee3631
= <dev.abidkk@gmail.com>
2024-08-06 05:46:55 -0700

    object

---
+++ Day7_Objects/A1.js
@@ -0,0 +1,16 @@
+// Objects
+
+// Taks: 1 
+let book =  {
+    title: "The Compont Effect",
+    author : "Darren Hardy",
+    year: 2002
+}
+
+console.log(book);
+
+// Task 2 : Access object properties
+console.log(book.title); // dot notation
+console.log(book["author"]); // square notation
+
+
