ffa3203c5fece50a9b0a2cc40ac6384fcfda40f0
= <dev.abidkk@gmail.com>
2024-08-06 06:50:18 -0700

    template literals

---
+++ Day8_ES6-Features/A1.js
@@ -0,0 +1,15 @@
+// Temepate Literals
+
+// Task 1
+let myName = "Abid Khokhar";
+let myAge = 24;
+console.log(`My name is ${myName}, I am ${myAge}.`);
+
+
+// Taks 2: Multi Line string
+
+let address = `North Karachi
+Sector 16 A 
+Pakistan
+`
+console.log(address);
